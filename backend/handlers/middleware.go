5447795a591ba5b3e6f105ada2d8a0beff65f20c
Igor <samptv59@gmail.com>
2025-05-01 15:46:54 +0300

    Update 1.5

--- backend/handlers/middleware.go
+++ backend/handlers/middleware.go
@@ -62,7 +62,7 @@ func JWTMiddleware(config *config.Configuration) gin.HandlerFunc {
 // CORSMiddleware создает middleware для обработки CORS
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173") // Адрес фронтенда
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://igorao79.github.io") // Адрес GitHub Pages
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
